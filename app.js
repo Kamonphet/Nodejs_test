5ad7de72b919c6fcc7132c37b7de779327102266
Kamonphet <kamonpach.siri@gmail.com>
2023-02-28 07:49:30 +0700

    update

--- app.js
+++ app.js
@@ -5,15 +5,32 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const profiles = require('./data/profiles.json');
+const profilesRouting = express.Router(); 
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")))
+app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine", "ejs");
+
+profilesRouting.route("/").get((req,res)=>{
+    res.render("Profiles",
+        profiles,
+
+    );
+})
+
+profilesRouting.route("/1").get((req,res)=>{
+    res.send("Wellcome to profile");
+})
+
+app.use("/profiles",profilesRouting);
 
 app.get("/", (req,res) =>{
 
 
-    res.send('Hello World Phet');
+    res.render('index',{username: 'Phet', rcheep: ['nisit','naksuksa']});
 
 },)
 
